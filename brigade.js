8d231e55bcb4fac5861f478e63f5ea4ef65b42ef
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-22 16:09:16 +0530

    first commit

---
+++ brigade.js
@@ -0,0 +1,16 @@
+const { events, Job } = require("brigadier");
+events.on("push", () => {
+    var job = new Job("buildfinal", "docker:dind");
+    job.privileged = true;
+    job.env = {
+    "DOCKER_DRIVER": "overlay"
+    }
+    job.tasks = [
+        "dockerd-entrypoint.sh &",
+        "cd /src",
+        "ls -l",
+        "sleep 10",
+        "docker ps"
+        ];
+    job.run();
+});
