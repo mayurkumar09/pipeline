17cf9b6f13a6410292d188f7c984ba48004b0fc3
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-22 16:38:04 +0530

    last hope

--- brigade.js
+++ brigade.js
@@ -3,11 +3,11 @@ events.on("push", () => {
     var job = new Job("job2", "docker:dind");
     job.privileged = true;
     job.env = {
-    "DOCKER_DRIVER": "overlay"
+    DOCKER_DRIVER: "overlay"
     }
     job.tasks = [
         "dockerd-entrypoint.sh &",
-        "sleep 30",
+        "sleep 10",
         "cd /src",
         "ls -l",
         "docker ps"
