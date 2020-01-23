6843775cd2179cae6eb1e4d2703b386c50cfe845
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-23 13:39:55 +0530

    commit id

--- brigade.js
+++ brigade.js
@@ -1,26 +1,28 @@
 const { events, Job } = require("brigadier");
-events.on("push", () => {
+events.on("push", (e, p) => {
     
     var greeting = new Job("job1", "alpine:latest")
     greeting.tasks = [
-    "echo Hello Pipeline"
+    "echo Hello Pipeline",
+    "console.log("Received push for commit " + e.revision.commit)"
+    
     ]
     
-    var docker = new Job("job2", "docker:dind");
-    docker.privileged = true;
-    docker.env = {
-    DOCKER_DRIVER: "overlay"
-    }
-    docker.tasks = [
-        "dockerd-entrypoint.sh &",
-        "sleep 10",
-        "cd /src",
-        "ls -l",
-        "docker build -t mayursuccessive/image-processing:v1 .",
-        "docker images"
-        ];
+    // var docker = new Job("job2", "docker:dind");
+    // docker.privileged = true;
+    // docker.env = {
+    // DOCKER_DRIVER: "overlay"
+    // }
+    // docker.tasks = [
+    //     "dockerd-entrypoint.sh &",
+    //     "sleep 10",
+    //     "cd /src",
+    //     "ls -l",
+    //     "docker build -t mayursuccessive/image-processing:v1 .",
+    //     "docker images"
+    //     ];
     
     greeting.run();
-    docker.run();
+    //docker.run();
 
 });
